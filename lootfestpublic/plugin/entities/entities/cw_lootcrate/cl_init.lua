e33f69a1edaff90c33fe02885b1ea464d98ba734
TacticalToaster <31429229+TacticalToaster@users.noreply.github.com>
2018-07-14 19:59:18 -0500

    Update cl_init.lua

--- lootfestpublic/plugin/entities/entities/cw_lootcrate/cl_init.lua
+++ lootfestpublic/plugin/entities/entities/cw_lootcrate/cl_init.lua
@@ -14,32 +14,9 @@ function ENT:HUDPaintTargetID(x, y, alpha)
 	local colorWhite = Clockwork.option:GetColor("white");
 	local crateName = self:GetDTString(0);
 	local crateDesc = self:GetDTString(1);
-	print(crateName, crateDesc)
 	
 	if (crateName && crateDesc) then
 		y = Clockwork.kernel:DrawInfo(crateName, x, y, colorTargetID, alpha);
 		y = Clockwork.kernel:DrawInfo(crateDesc, x, y, colorWhite, alpha);
 	end;
 end;
-
-/*
--- Called when the entity should draw.
-function ENT:Draw()
-	if (!Clockwork.entity:HasFetchedItemData(self)) then
-		return;
-	end;
-	
-	local drawModel = true;
-	local itemTable = Clockwork.entity:FetchItemTable(self);
-	local shouldDrawItemEntity = Clockwork.plugin:Call("ItemEntityDraw", itemTable, self);
-	
-	if (shouldDrawItemEntity == false
-	or (itemTable.OnDrawModel and itemTable:OnDrawModel(self) == false)) then
-		drawModel = false;
-	end;
-	
-	if (drawModel) then
-		self:DrawModel();
-	end;
-end;
-*/
